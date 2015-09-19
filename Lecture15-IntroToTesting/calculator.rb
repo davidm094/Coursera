8cdd41f9daf23f07ece6ff7f4e8dc912acff8f2d
David Moreno <davidm094@gmail.com>
2015-09-19 12:46:15 -0430

    testing

--- Lecture15-IntroToTesting/calculator.rb
+++ Lecture15-IntroToTesting/calculator.rb
@@ -19,3 +19,5 @@ class Calculator
   end
 end
 
+
+
--- Lecture15-IntroToTesting/calculator_test.rb
+++ Lecture15-IntroToTesting/calculator_test.rb
@@ -1,9 +1,15 @@
-require 'test/unit'
+require 'minitest/autorun'
 require_relative 'calculator'
 
-class CalculatorTest < Test::Unit::TestCase
+class CalculatorTest < Minitest::Test
   def setup
   	@calc = Calculator.new('test')
+  end
+  def test_addition
+  	assert_equal 8, @calc.add(4,4)
+  end
+  def test_substraction
+  	assert_equal 8, @calc.substration(12,4)
   end
 
   def test_divide_by_zero
